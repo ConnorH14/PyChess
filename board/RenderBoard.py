e194ebc3aa388b9117fbf3201ed776ca7fa61f52
ConnorH14 <ConnorH14@live.com>
2024-06-07 15:47:52 -0600

    Adding icon to chess window

---
+++ board/RenderBoard.py
@@ -0,0 +1,5 @@
+import pygame
+
+
+def render_board(screen):
+    screen.fill("darkgrey")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from board.RenderBoard import render_board
 
 # Setup to draw screen
 pygame.init()
@@ -6,17 +7,21 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
 
+# Setup window customization
+pygame.display.set_caption("PyChess")
+pygame.display.set_icon(pygame.image.load("assets/chess_icon.png"))
+
 
 # Run game loop
 while running:
 
-    # Exit window when 'X' is pressed
+    # Exit window when 'X' is clicked
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Wipe screen to draw next frame
-    screen.fill("darkgrey")
+    render_board(screen)
 
     # Prints work to the screen
     pygame.display.flip()
